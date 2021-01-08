cc65ea69f3c445e765ebfa90cd15672f8c7e35a6
PascalPolygon <pdao2015@my.fit.edu>
2021-01-08 06:15:04 -0500

    str2hex & bad start freq code computation

--- inc/AD5933_EB/AD5933_EB.cpp
+++ inc/AD5933_EB/AD5933_EB.cpp
@@ -17,3 +17,41 @@ bool AD5933_EB::reset(void)
     else
         return true;
 }
+
+void AD5933_EB::set_start_freq(float freq)
+{
+    //Start freq code:= (required_req/(MCLK/4))*2^27
+    char code_buf[24];
+    _start_freq = freq;
+    // int D = int(freq / (MCLK / 4.0)) * pow(2, 27);
+    long int D = int(1950 / (16776000 / 4.0)) * pow(2, 27);
+    Serial.println(D, DEC);
+    String str = String(D, HEX);
+    Serial.println(str);
+    str.toCharArray(code_buf, 24);
+    int freq_code = str2hex(code_buf);
+    Serial.println(freq_code);
+}
+
+int AD5933_EB::str2hex(char *s)
+{
+    int x = 0;
+    for (;;)
+    {
+        char c = *s;
+        if (c >= '0' && c <= '9')
+        {
+            x *= 16;
+            x += c - '0';
+        }
+        else if (c >= 'A' && c <= 'F')
+        {
+            x *= 16;
+            x += (c - 'A') + 10;
+        }
+        else
+            break;
+        s++;
+    }
+    return x;
+}
--- inc/AD5933_EB/AD5933_EB.h
+++ inc/AD5933_EB/AD5933_EB.h
@@ -10,12 +10,18 @@
 #include "Arduino.h"
 
 #define AD5933_ADDR (0x0D)
+#define MCLK (16776000) //16.776 MHz internal oscillator on (AD5933)
 
 class AD5933_EB
 {
 public:
   AD5933_EB();
   bool reset(void);
+  void set_start_freq(float freq);
+
+private:
+  float _start_freq;
+  int str2hex(char *s);
 };
 
 #endif
